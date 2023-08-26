d6b802e5a6cdf874f7ab8a62320bff2980845ed6
icarso <igor.c.souza@viannasempre.com.br>
2023-08-26 19:57:06 -0300

    Ajustes Gerais + Comunicação com API (backend)

--- js/main.js
+++ js/main.js
@@ -1,24 +1,47 @@
-const axios = require('axios');
+let respostaUsuario;
+const nome = document.getElementById("nome_box");
+const email = document.getElementById("email_box");
+const telefone = document.getElementById("telefone_box");
+const mensagem = document.getElementById("message_box");
 
-const nome = document.getElementById("nome_box").value;
-const email = document.getElementById("email_box").value;
-const telefone = document.getElementById("telefone_box").value;
-const mensagem = document.getElementById("message_box").value;
 
 function enviarFormulario() {
+
     axios.post('http://localhost:5196/api/contato', {
-        "nome": nome,
-        "email": email,
-        "telefone": telefone,
-        "mensagem": mensagem
+        "nome": nome.value,
+        "email": email.value,
+        "telefone": telefone.value,
+        "mensagem": mensagem.value
     })
-        .then(response => console.log(response))
-        .catch(error => error == 405? "Ok": "false")
+        .then(response => {
+            if (response.status === 200){
+                respostaUsuario = document.getElementById('alertaSucesso');
+                respostaUsuario.classList.add('show');
+                fecharAutomatico(respostaUsuario);
+            } else {
+                respostaUsuario = document.getElementById('alertaErro');
+                respostaUsuario.classList.add('show');
+                fecharAutomatico(respostaUsuario);
+            }
+        })
+        .catch(error => {
+            respostaUsuario = document.getElementById('alertaErro');
+            respostaUsuario.classList.add('show')
+            , console.log(error)
+        });
+
+        resetar();
 }
 
 function resetar(){
-    nome.value == null;
-    email.value == null;
-    telefone.value == null;
-    mensagem.value == null;
+    nome.value = null;
+    email.value = null;
+    telefone.value = null;
+    mensagem.value = null;
+}
+
+function fecharAutomatico(respostaUsuario){
+    setTimeout(() => {
+        respostaUsuario.style.display = 'none';
+    }, 5000);
 }
